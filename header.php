1f043da73d9627a19b28953fcb58454616db2355
empthinking <117464630+empthinking@users.noreply.github.com>
2024-10-24 15:45:24 -0300

    Ajuste no tamanho da logo

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <nav class="navbar navbar-dark">
     <div class="container">
       <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-      <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-mania-logo.png" alt="Logotipo"> </a>
+      <a class="navbar-brand" href="index.php"> <img class="img-rounded" height="50px" src="images/food-mania-logo.png" alt="Logotipo"> </a>
       <div class="collapse navbar-toggleable-md float-lg-right" id="mainNavbarCollapse">
         <ul class="nav navbar-nav">
           <li class="nav-item"> <a class="nav-link active" href="index.php">Início <span class="sr-only">(atual)</span></a> </li>
