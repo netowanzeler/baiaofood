5c7e6b538a976ec5656c59602da62198061c5d2c
VINICIUS GONCALVES RIBAS <101434879+Viniciusp2@users.noreply.github.com>
2024-10-31 22:09:18 -0300

    Create config.php

---
+++ pix/config.php
@@ -0,0 +1,10 @@
+<?php
+// Código por: github.com/mths1901
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// https://www.mercadopago.com.br/settings/account/credentials
+$acess_token = "TEST-7529557071731731-101100-6b5ea8db605caf2697d4164086663acf-232154518";
+
+
+$notification_url = "https://lightskyblue-owl-392240.hostingersite.com";
